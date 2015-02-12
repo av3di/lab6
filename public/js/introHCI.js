68167c585bbd698b2ff69d56fa22c37c4b16ac3d
Evonee Uribe <ekuribe@ucsd.edu>
2015-02-12 22:22:18 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,6 +17,15 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+ function callBack(result)
+{
+	console.log(result);
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p><p>' + result['summary'] + '</a>';
+	var proj_div = 'div#project' + result['id'] + ' div.thumbnail div.details';
+	console.log("selector is " + proj_div);
+	$(proj_div).html(projectHTML);
+}
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -25,14 +34,31 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	$.get("/project/" + idNumber, callBack);
 	console.log("User clicked on project " + idNumber);
+	console.log("Url: /project/" + idNumber);
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
+ function colorCallBack(result)
+ {
+ 	var hex = result['colors'];
+ 	var colors = hex['hex'];
+ 	$('body').css('background-color', colors[0]);
+ 	$('.thumbnail').css('background-color', colors[1]);
+ 	$('h1, h2, h3, h4, h5, h6').css('color', colors[2]);
+ 	$('p').css('color', colors[3]);
+ 	$('.project img').css('opacity', 0.75);
+ }
+
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	e.preventDefault();
+
+	$.get("/palette", colorCallBack);
 }
